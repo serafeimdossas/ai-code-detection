f1a136042287973e5fef8177f5dd716b763d1717
Serafeim Dossas <serafeimdossas@gmail.com>
2025-07-16 09:26:48 +0000

    predict_oneoff minor change

--- src/models/predict_oneoff.py
+++ src/models/predict_oneoff.py
@@ -10,20 +10,6 @@ le   = joblib.load("models/xgb_baseline_label_encoder.pkl")
 
 # 2) Your one-off code snippet
 snippet = '''
-	
-from decimal import *
- 
-D = Decimal
-getcontext().prec = 100
-a = n = D(1)
-g, z, half = 1 / D(2).sqrt(), D(0.25), D(0.5)
-for i in range(18):
-x = [(a + g) * half, (a * g).sqrt()]
-var = x[0] - a
-z -= var * var * n
-n += n
-a, g = x
-print(a * a / z)
 '''
 
 # 3) Clean & vectorize
