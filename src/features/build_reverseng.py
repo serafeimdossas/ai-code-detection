4e8bace5b69930526314375255e2bf7083e0f974
Serafeim Dossas <serafeimdossas@gmail.com>
2025-09-15 11:12:02 +0000

    calculate max fused value method implemented

--- src/features/build_reverseng.py
+++ src/features/build_reverseng.py
@@ -4,6 +4,7 @@ import os, json
 from datasets import load_dataset
 from src.utils.code_requirements_extraction import analyze_source, save_spec, Spec
 from src.utils.generate_llm_code import gen_candidates
+from src.utils.code_comparison import compare_against_candidates
 
 DATASET_NAME = "serafeimdossas/ai-code-detection"
 
@@ -11,11 +12,15 @@ DATASET_NAME = "serafeimdossas/ai-code-detection"
 SPECS_FOLDER = "artifacts/specs"
 CANDIDATES_FOLDER = "artifacts/candidates"
 CACHE_FOLDER = "artifacts/cache"
+ORIGINAL_FOLDER = "artifacts/original"
 
 # LLMs configuration
 LLM_MODELS = ["openai:gpt-4o-mini"] # e.g., "openai:gpt-4o-mini", "anthropic:claude-3-5-sonnet", "vertex:codegemini"
 TEMPS = [0.0, 0.7]                  # deterministic + diverse
 K = 2                               # candidates per (model, temp)
+
+# weights for fused value calculation
+WEIGHTS = {"lev": 0.15, "tok_jacc": 0.15, "ast": 0.25, "sem": 0.45}
 
 # Loads samples from the specified dataset
 def load_samples_from_dataset(number_of_samples=None):
@@ -34,6 +39,9 @@ def extract_code_requirements(samples):
     # array to store occured errors for future adjustments 
     errors = []
 
+    # array for storing snippet_id and their labels
+    result = []
+
     # keep count of spec files created
     count = 0
     
@@ -43,25 +51,38 @@ def extract_code_requirements(samples):
         if i % 10 == 0:
             print(f"Processing sample {i}/{len(samples)}")
         
+        # get current code and its label
+        code = sample["code"]
+        label = sample["label"]
+        
         try:
             # analyze code of sample
-            code = sample["code"]
             requirements = analyze_source(code)
         except Exception as e:
             # error occured, mone on to next sample
             errors.append(str(e))
             # print(f"Error analyzing sample {sample['task_name']}: {e}")
             continue
-
-        # print(f"{sample['task_name']} - {requirements.snippet_id}")
         
         # save spec file only when requirements of code were extracted
         if len(requirements.requirements) > 0:
+            # save spec file
             save_spec(requirements, SPECS_FOLDER)
+            
+            # save original code
+            original_code_path = f"{ORIGINAL_FOLDER}/{requirements.snippet_id}.py"
+            with open(original_code_path, "w") as f:
+                f.write(code)
+
+            # update result array and counter
+            result.append({"snippet_id": requirements.snippet_id, "label": label})
             count += 1
 
     print(f"Finished processing {len(samples)} samples with {len(errors)} errors.")
     print(f"Total spec files created: {count}")
+    
+    # return array of ids for codes successfully analyzed along with their labels
+    return result
 
 def generate_ai_code():
     # find specs folder and files
@@ -83,15 +104,46 @@ def generate_ai_code():
         # experimental break of loop
         break
 
+def calculate_fused_values(snippet_ids):
+    # parse array
+    for i, item in enumerate(snippet_ids):
+        # construct original and candidates file names for current snippet id
+        candidates_paths = [f"{CANDIDATES_FOLDER}/{item["snippet_id"]}__{model}__t{temp}__k{k}.py" for model in LLM_MODELS for temp in TEMPS for k in range(K)]
+        original_path = f"{ORIGINAL_FOLDER}/{item["snippet_id"]}.py"
+
+        # compare original code to candidates
+        per_candidate = compare_against_candidates(original_path, candidates_paths)
+
+        # calculate fused value
+        for pc in per_candidate:
+            fused_value = sum(pc["scores"][metric] * WEIGHTS[metric] for metric in WEIGHTS)
+            pc["fused"] = fused_value
+
+        # get max fused value
+        fused_values = [pc["fused"] for pc in per_candidate]
+        max_fused = max(fused_values) if fused_values else 0.0
+
+        # update array to include max_fused value
+        snippet_ids[i]["max_fused"] = max_fused
 
 def main():
-    # extract code requirements from dataset samples and save as spec files
+    # get code samples from dataset
     number_of_samples = 10
     code_samples = load_samples_from_dataset(number_of_samples)
-    extract_code_requirements(code_samples)
 
-    # generate candidates spec files created
+    # extract code requirements from dataset code samples and save as spec files
+    # response contains snippet ids and their labels for codes successfully analyzed
+    snippet_ids = extract_code_requirements(code_samples)
+
+    # generate candidates codes for the created spec files
     generate_ai_code()
+
+    # calculate max fused value for analyzed codes and add to existing array
+    calculate_fused_values(snippet_ids)
+
+    # print final results
+    print("Final results:")
+    print(snippet_ids)
 
 if __name__ == "__main__":
     main()
